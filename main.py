530e5f34c30416b34616be56ee393e5bb2403036
DMax <maksim.dvornik.2017@gmail.com>
2021-11-15 18:02:32 +0300

    Create main working path of the program
    Add logging to the starter main() function of the program
    and describe the main sequence of actions to complete this task.
    
    Here we need logging to underline the begin and end moments
    of the current work process.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import time
+import os
+import logging
+from parse_main_functions import parser
 
 
 def get_current_filename() -> str:
@@ -18,9 +21,34 @@ def get_current_filename() -> str:
     return 'reddit-' + ''.join(time_list) + '.txt'
 
 
+URL = 'https://www.reddit.com/top/?t=month'
+NUM_POSTS = 100
+
+
 def main() -> None:
     """Run main application's functions."""
     file_name = get_current_filename()
+
+    if not os.path.isdir("logging_data"):
+        os.mkdir("logging_data")
+
+    logger = logging.getLogger("parserApp")
+    logger.setLevel(logging.DEBUG)
+
+    # create the logging file handler
+    fh = logging.FileHandler(f"logging_data\\logger - {file_name.replace('txt', '')}.log")
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+
+    # add handler to logger object
+    logger.addHandler(fh)
+
+    logger.info("Program started.")
+    file = open(file_name, 'w')
+    file.close()
+    parser(URL, file_name, NUM_POSTS)
+    logger.info("Program finished.")
 
 
 if __name__ == '__main__':
