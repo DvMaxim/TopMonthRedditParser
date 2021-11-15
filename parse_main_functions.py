d8c134753f528349d7c7fdbb7b89c6469379d080
DMax <maksim.dvornik.2017@gmail.com>
2021-11-15 17:55:37 +0300

    Add module for the main functions of the program

---
+++ parse_main_functions.py
@@ -0,0 +1,24 @@
+"""The module for the functions which have the main functional.
+
+:func filter_post_list: produce filter process over the list of posts
+:func beta_parser: test parsing process on some single post
+:func parser: launch parser
+"""
+
+import logging
+from selenium import webdriver
+
+
+module_logger = logging.getLogger("parserApp.parse_main_functions")
+
+
+def filter_post_list():
+    pass
+
+
+def beta_parser():
+    pass
+
+
+def parser():
+    pass
